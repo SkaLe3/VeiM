b25285f4c0b96eff12a030275154885899c21ce4
SkaLe3 <39skale@gmail.com>
2025-01-17 23:17:17 +0200

    Test game project, still not usable

---
+++ Samples/TestVeiMGame/Source/TestVeiMGame/Game/TestVeiMGame.cpp
@@ -0,0 +1,6 @@
+#include "TestVeiMGame.h"
+
+
+
+#include "main.h"
+
---
+++ Samples/TestVeiMGame/Source/TestVeiMGame/Game/main.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Main output" << std::endl;  
+    return 0;
+}
